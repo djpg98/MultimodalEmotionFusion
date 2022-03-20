91ca4067f257ab6c3013600c4670e33c201f0b80
Diego Peña <djpg98@gmail.com>
2022-03-20 19:21:56 -0400

    Spelling, renaming, bug fix

--- run_n_iterations.py
+++ run_n_iterations.py
@@ -3,23 +3,30 @@ import sys
 
 from os.path import join, exists
 
-METHOD_LIST = [
-    'mlp_simple',
-    'attention_mlp'
-]
+METHOD_SCRIPTS = {
+    'mlp_simple': 'test_mlp.py',
+    'attention_mlp': 'test_attention.py'
+}
 
 """
 INSTRUCTIONS:
     This script requires three arguments and has an optional one:
     - method: Fusion method to be used
-    - architecture: Fusion architecture to be used (Available architectures are listed at Architectures/architectures.py)
+    - architechture: Fusion architechture to be used (Available architectures are listed at Architechtures/architechtures.py)
     - iterations: Number of iterations
     - weighted (Optional): If passed a Weighted loss function is used (Pass "-w" as the last argument).  
 """
 
 method = sys.argv[1]
-architecture = sys.argv[2]
+architechture = sys.argv[2]
 iterations = int(sys.argv[3])
+
+if method not in METHOD_SCRIPTS.keys():
+    formated_method_list = ", ".join(METHOD_SCRIPTS.keys())
+    print(f"Error: Selected fusion method does not exist. Try with one of the following: {formated_method_list}")
+    sys.exit(-1)
+
+script = METHOD_SCRIPTS[method]
 
 if (len(sys.argv) == 5 and sys.argv[4] == '-w'):
     directory = 'weighted'
@@ -28,10 +35,6 @@ else:
     directory = 'unweighted'
     weight = False
 
-if method not in METHOD_LIST:
-    formated_method_list = ", ".join(METHOD_LIST)
-    print(f"Error: Selected fusion method does not exist. Try with one of the following: {formated_method_list}")
-    sys.exit(-1)
 
 # These are the paths where results and saved models will be saved at first
 original_plots_path = join('Results', method, 'Plots')
@@ -39,9 +42,9 @@ original_data_path = join('Results', method, 'Training Data')
 original_saved_models_path = join('Saved Models')
 
 # Check existence of new appropiate directories for results/saved models
-plots_path = join(original_plots_path, architecture, directory)
-data_path = join(original_data_path, architecture, directory)
-saved_models_path = join(original_saved_models_path, method, architecture, directory)
+plots_path = join(original_plots_path, architechture, directory)
+data_path = join(original_data_path, architechture, directory)
+saved_models_path = join(original_saved_models_path, method, architechture, directory)
 
 if not exists(plots_path):
     os.makedirs(plots_path)
@@ -55,15 +58,15 @@ if not exists(saved_models_path):
 for i in range(iterations):
 
     if weight:
-        status = os.system(f"python test_attention.py {architecture} 0 -w")
+        status = os.system(f"python {script} {architechture} 0 -w")
     else:
-        status = os.system(f"python test_attention.py {architecture} 0")
+        status = os.system(f"python {script} {architechture} 0")
 
     if status == -1:
         sys.exit(-1)
     else:
         #Renaming and moving
-        base_name = f'model_{architecture}_adam'
+        base_name = f'model_{architechture}_adam'
         
         if weight:
             suffix = f'w_{i+1}'
