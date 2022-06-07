7f4559dd6e5d20c53b730a276ee886fe4a591516
Diego Peña <djpg98@gmail.com>
2022-06-07 10:55:28 -0400

    Table generation refactor, F1 support, new stats, and additional comments

--- make_table.py
+++ make_table.py
@@ -16,15 +16,29 @@ METHOD_LIST = [
     'embracenet'
 ]
 
-weighted_loss_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': []}
-weighted_acc_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': []}
-weighted_loss_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': []}
-weighted_acc_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': []}
+weighted_loss_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+weighted_acc_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+weighted_loss_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+weighted_acc_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+weighted_f1_macro_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+weighted_f1_weighted_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+weighted_f1_macro_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+weighted_f1_weighted_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
 
-unweighted_loss_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': []}
-unweighted_acc_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': []}
-unweighted_loss_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': []}
-unweighted_acc_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': []}
+weighted_metrics = [weighted_loss_train, weighted_acc_train, weighted_loss_val, weighted_acc_val, 
+                    weighted_f1_macro_train, weighted_f1_weighted_train, weighted_f1_macro_val, weighted_f1_weighted_val]
+
+unweighted_loss_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+unweighted_acc_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+unweighted_loss_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+unweighted_acc_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+unweighted_f1_macro_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+unweighted_f1_weighted_train = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+unweighted_f1_macro_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+unweighted_f1_weighted_val = {'model': [], 'repetitions': [], 'avg': [], 'min': [], 'max': [], 'var': []}
+
+unweighted_metrics = [unweighted_loss_train, unweighted_acc_train, unweighted_loss_val, unweighted_acc_val, 
+                      unweighted_f1_macro_train, unweighted_f1_weighted_train, unweighted_f1_macro_val, unweighted_f1_weighted_val]
 
 method = sys.argv[1]
 data_path = join('Results', method, 'Training Data')
@@ -47,13 +61,17 @@ for model_dir in os.listdir(encoded_path):
         train_acc = []
         val_loss = []
         val_acc = []
+        train_f1_macro = []
+        train_f1_weighted = []
+        val_f1_macro = []
+        val_f1_weighted = []
 
-        iterate_model_results(encoded_iter_dir, weighted_path, train_loss, train_acc, val_loss, val_acc)
+        weighted_lists = [train_loss, train_acc, val_loss, val_acc, train_f1_macro, train_f1_weighted, val_f1_macro,val_f1_weighted]
 
-        add_data(weighted_loss_train, decoded_model, train_loss)
-        add_data(weighted_loss_val, decoded_model, val_loss)
-        add_data(weighted_acc_train, decoded_model, train_acc)
-        add_data(weighted_acc_val, decoded_model, val_acc)
+        iterate_model_results(encoded_iter_dir, weighted_path, train_loss, train_acc, val_loss, val_acc, train_f1_macro, train_f1_weighted, val_f1_macro, val_f1_weighted)
+
+        for stats_row, data in zip(weighted_metrics, weighted_lists):
+            add_data(stats_row, decoded_model, data)
 
     unweighted_path = join(model_path, 'unweighted')
 
@@ -65,43 +83,60 @@ for model_dir in os.listdir(encoded_path):
         train_acc = []
         val_loss = []
         val_acc = []
+        train_f1_macro = []
+        train_f1_weighted = []
+        val_f1_macro = []
+        val_f1_weighted = []
 
-        iterate_model_results(encoded_iter_dir, unweighted_path, train_loss, train_acc, val_loss, val_acc)
+        unweighted_lists = [train_loss, train_acc, val_loss, val_acc, train_f1_macro, train_f1_weighted, val_f1_macro,val_f1_weighted]
 
-        add_data(unweighted_loss_train, decoded_model, train_loss)
-        add_data(unweighted_loss_val, decoded_model, val_loss)
-        add_data(unweighted_acc_train, decoded_model, train_acc)
-        add_data(unweighted_acc_val, decoded_model, val_acc)
+        iterate_model_results(encoded_iter_dir, unweighted_path, train_loss, train_acc, val_loss, val_acc, train_f1_macro, train_f1_weighted, val_f1_macro, val_f1_weighted)
 
+        for stats_row, data in zip(unweighted_metrics, unweighted_lists):
+            add_data(stats_row, decoded_model, data)
 
-df_weighted_loss_train = pd.DataFrame(data=weighted_loss_train)
-df_weighted_acc_train = pd.DataFrame(data=weighted_acc_train)
-df_weighted_loss_val = pd.DataFrame(data=weighted_loss_val)
-df_weighted_acc_val = pd.DataFrame(data=weighted_acc_val)
+df_names = ['loss_train', 'acc_train', 'loss_val', 'acc_val', 'f1_macro_train', 'f1_weighted_train', 'f1_macro_val', 'f1_weighted_val']
 
-df_unweighted_loss_train = pd.DataFrame(data=unweighted_loss_train)
-df_unweighted_acc_train = pd.DataFrame(data=unweighted_acc_train)
-df_unweighted_loss_val = pd.DataFrame(data=unweighted_loss_val)
-df_unweighted_acc_val = pd.DataFrame(data=unweighted_acc_val)
+df_weighted = {}
 
-df_weighted_loss_train = add_columns_mean(df_weighted_loss_train)
-df_weighted_acc_train = add_columns_mean(df_weighted_acc_train)
-df_weighted_loss_val = add_columns_mean(df_weighted_loss_val)
-df_weighted_acc_val = add_columns_mean(df_weighted_acc_val)
+for name, data in zip(df_names, weighted_metrics):
 
-df_unweighted_loss_train = add_columns_mean(df_unweighted_loss_train)
-df_unweighted_acc_train = add_columns_mean(df_unweighted_acc_train)
-df_unweighted_loss_val = add_columns_mean(df_unweighted_loss_val)
-df_unweighted_acc_val = add_columns_mean(df_unweighted_acc_val)
+    df_weighted[name] = pd.DataFrame(data=data)
 
-with pd.ExcelWriter(join('Tables', f'{method}_tables.xlsx')) as writer:
+df_unweighted = {}
 
-    df_weighted_loss_train.to_excel(writer, sheet_name='Weighted Loss Train')
-    df_weighted_acc_train.to_excel(writer, sheet_name='Weighted Acc Train')
-    df_weighted_loss_val.to_excel(writer, sheet_name='Weighted Loss Val')
-    df_weighted_acc_val.to_excel(writer, sheet_name='Weighted Acc Val')
+for name, data in zip(df_names, unweighted_metrics):
 
-    df_unweighted_loss_train.to_excel(writer, sheet_name='Unweighted Loss Train')
-    df_unweighted_acc_train.to_excel(writer, sheet_name='Unweighted Acc Train')
-    df_unweighted_loss_val.to_excel(writer, sheet_name='Unweighted Loss Val')
-    df_unweighted_acc_val.to_excel(writer, sheet_name='Unweighted Acc Val')
+    df_unweighted[name] = pd.DataFrame(data=data)
+
+for key, data in df_weighted.items():
+
+    df_weighted[key] = add_columns_mean(df_weighted[key])
+
+for key, data in df_unweighted.items():
+
+    df_unweighted[key] = add_columns_mean(df_unweighted[key])
+
+with pd.ExcelWriter(join('Tables', f'{method}_tables_train.xlsx')) as writer:
+
+    df_weighted['loss_train'].to_excel(writer, sheet_name='Weighted Loss Train')
+    df_weighted['acc_train'].to_excel(writer, sheet_name='Weighted Acc Train')
+    df_weighted['f1_macro_train'].to_excel(writer, sheet_name='Weighted F1 Macro Train')
+    df_weighted['f1_weighted_train'].to_excel(writer, sheet_name='Weighted F1 Weighted Train')
+
+    df_unweighted['loss_train'].to_excel(writer, sheet_name='Unweighted Loss Train')
+    df_unweighted['acc_train'].to_excel(writer, sheet_name='Unweighted Acc Train')
+    df_unweighted['f1_macro_train'].to_excel(writer, sheet_name='Unweighted F1 Macro Train')
+    df_unweighted['f1_weighted_train'].to_excel(writer, sheet_name='Unweighted F1 Weighted Train')
+
+with pd.ExcelWriter(join('Tables', f'{method}_tables_val.xlsx')) as writer: 
+
+    df_weighted['loss_val'].to_excel(writer, sheet_name='Weighted Loss Val')
+    df_weighted['acc_val'].to_excel(writer, sheet_name='Weighted Acc Val')
+    df_weighted['f1_macro_val'].to_excel(writer, sheet_name='Weighted F1 Macro Val')
+    df_weighted['f1_weighted_val'].to_excel(writer, sheet_name='Weighted F1 Weighted Val')
+
+    df_unweighted['loss_val'].to_excel(writer, sheet_name='Unweighted Loss Val')
+    df_unweighted['acc_val'].to_excel(writer, sheet_name='Unweighted Acc Val')
+    df_unweighted['f1_macro_val'].to_excel(writer, sheet_name='Uneighted F1 Macro Val')
+    df_unweighted['f1_weighted_val'].to_excel(writer, sheet_name='Unweighted F1 Weighted Val')
